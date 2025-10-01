b99fa6d70294d3091db5e1ad5fe2899a3cdf237b
Sarah Beatriz <sarah.nascimento02052007@gmail.com>
2025-10-01 11:09:38 +0000

    questao 10

--- Analise_de_algoritmos/algoritmo1.cpp
+++ Analise_de_algoritmos/algoritmo1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 bool primo(int n){
     int div = 0;
-    for (int i = 2; i <= n; ++i){
+    for (int i = 1; i <= n; ++i){
         if (n % i == 0){
             div = div + 1;
         }
